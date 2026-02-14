90c5558ebefef98bc8ae4eeb2a3dc07c03d9a363
razoring <blotzytgaming@gmail.com>
2026-02-14 03:03:00 -0500

    load model and set up env

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
+import sys
+import os
+from dotenv import load_dotenv as load
 import torch
 import librosa
 import numpy as np
 from fastapi import FastAPI, UploadFile, File
 from transformers import AutoFeatureExtractor, AutoModelForAudioClassification
+
+load()
+app = FastAPI()
+model = os.getenv("DATASET")
