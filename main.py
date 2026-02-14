aa066a85a64dac94713c343cf9ecffd33b5806d3
razoring <blotzytgaming@gmail.com>
2026-02-14 03:37:55 -0500

    post test

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import librosa
 import numpy as np
 from fastapi import FastAPI, UploadFile, File
 from transformers import AutoFeatureExtractor, AutoModelForAudioClassification
+import uvicorn
 
 load()
 app = FastAPI()
@@ -19,7 +20,12 @@ try:
     model.eval()
 except Exception as e: traceback.print_exc()
 
-file:File = "AbeBurrows_sample.wav"
-with open(file,"wb") as buffer: buffer.write(file.read())
-speech, rate = librosa.load(file, sr=16000) #16000 current sample rate
-inputs = extract(speech, sampling_rate=rate, return_tensors="pt", padding=True, max_length=rate*30, truncation=True) #!!! rate*30 = 16000*30 seconds of processing MAX
+@app.post("/predict")
+async def train():
+    file:File = "AbeBurrows_sample.wav"
+    with open(file,"wb") as buffer: buffer.write(file.read())
+    speech, rate = librosa.load(file, sr=16000) #16000 current sample rate
+    inputs = extract(speech, sampling_rate=rate, return_tensors="pt", padding=True, max_length=rate*30, truncation=True) #!!! rate*30 = 16000*30 seconds of processing MAX
+
+# DO NOT TOUCH BRO
+if __name__ == "__main__": uvicorn.run("main:app", host="localhost", port=6967)
