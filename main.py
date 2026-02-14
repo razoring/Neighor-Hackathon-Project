ecec6f26e970c4ecf5ece82f91d0886a484f3669
raymondso123 <blotzytgaming@gmail.com>
2026-02-14 14:47:05 -0500

    unsure if working

--- main.py
+++ main.py
@@ -232,4 +232,4 @@ async def analyze_session(session_id: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
